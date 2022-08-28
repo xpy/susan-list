7e745a43f642ac2b354b1e50b37d9848f8118ba5
Pavloschris <pavloschris@gmail.com>
2022-08-28 18:33:58 +0300

    Updates

---
+++ _app/immutable/chunks/seo-406ddecf.js
@@ -0,0 +1,1 @@
+import{w as t}from"./index-cc486d72.js";const a={defaultTitle:"Susan list - Portrait artist/Illustrator",defaultDescription:"Susan list is a Photorealistic Portrait artist and Illustrator from the Netherlands"},r=t(""),i=t("");export{a as c,i as d,r as t};
---
+++ _app/immutable/pages/art/index.svelte-4e7014cd.js
@@ -0,0 +1,1 @@
+import{S as s,i as e,s as r}from"../../chunks/index-23d67bfb.js";function o(){return{status:302,redirect:"/art/0"}}class l extends s{constructor(t){super(),e(this,t,null,null,r,{})}}export{l as default,o as load};
